b7ee3463d33a27d1017e2a7069c92c94ce9083f0
stanimeros <pantelisstanimeros@outlook.com>
2024-07-26 17:44:28 +0300

    set light mode as default

--- lib/common/theme_provider.dart
+++ lib/common/theme_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ThemeProvider with ChangeNotifier {
-  late ThemeMode _themeMode = ThemeMode.system;
+  late ThemeMode _themeMode = ThemeMode.light;
   
   ThemeMode get themeMode => _themeMode;
   void setThemeMode(ThemeMode value) {
