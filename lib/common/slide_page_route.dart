1f049767792fd377eae2ba0786a06c685fce1a49
stanimeros <pantelisstanimeros@outlook.com>
2024-08-07 19:00:58 +0300

    slide page route update

--- lib/common/slide_page_route.dart
+++ lib/common/slide_page_route.dart
@@ -4,26 +4,30 @@ class SlidePageRoute<T> extends PageRouteBuilder<T> {
   final Widget page;
   final Curve curve;
 
-  SlidePageRoute({required this.page, this.curve = Curves.easeInOut})
-    : super(
-      pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
-        return page;
-      },
-      transitionDuration: const Duration(milliseconds: 300),
-      reverseTransitionDuration: const Duration(milliseconds: 300),
-      transitionsBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
-        return SlideTransition(
-          position: Tween<Offset>(
-            begin: const Offset(1.0, 0.0),
-            end: Offset.zero,
-          ).animate(
-            CurvedAnimation(
-              parent: animation,
-              curve: curve,
-            ),
-          ),
-          child: child,
-        );
-      },
-    );
+  SlidePageRoute({
+    required this.page,
+    this.curve = Curves.easeInOut,
+  }) : super(
+    pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
+      return page;
+    },
+    transitionDuration: const Duration(milliseconds: 300),
+    reverseTransitionDuration: const Duration(milliseconds: 300),
+    transitionsBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+      final Animation<Offset> offsetAnimation = Tween<Offset>(
+        begin: const Offset(1.0, 0.0),
+        end: Offset.zero,
+      ).animate(
+        CurvedAnimation(
+          parent: animation,
+          curve: curve,
+        ),
+      );
+
+      return SlideTransition(
+        position: offsetAnimation,
+        child: child,
+      );
+    },
+  );
 }
